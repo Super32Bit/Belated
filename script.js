496b42cbb00e318887ea52d58f242d3303b9d468
Super32Bit <pipo.kub@gmail.com>
2025-03-20 00:09:50 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const name = "name";
-document.getElementById("Nick'name Noo").innerText = name;
+document.getElementById("name").innerText = name;
 
 const quotes = [
   "You are my today and all of my tomorrows. ❤️",
